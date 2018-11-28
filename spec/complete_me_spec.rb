f6f52994989aecb1ea079d13b05119ea679bf6f5
jtrtj <jt.r@live.com>
2018-11-28 13:48:58 -0700

    Write test for suggest instance method

--- spec/complete_me_spec.rb
+++ spec/complete_me_spec.rb
@@ -1,18 +1,24 @@
 require 'spec_helper'
 
 describe CompleteMe do
+  before(:each) do
+    @completion = CompleteMe.new
+  end
   it 'exists' do
-    completion = CompleteMe.new
-
-    expect(completion).to be_a(CompleteMe)
+    expect(@completion).to be_a(CompleteMe)
   end
 
   context 'instance methods' do
     it '#insert - adds string to @list and #count returns the count of strings in @list' do
-      completion = CompleteMe.new
-      completion.insert("pizza")
+      @completion.insert("pizza")
 
-      expect(completion.count).to eq(1)
+      expect(@completion.count).to eq(1)
+    end
+
+    it '#suggest - returns suggestions of words based on string input' do
+      @completion.insert("pizza")
+      
+      expect(@completion.suggest("piz")).to eq(["pizza"])
     end
   end
 end
