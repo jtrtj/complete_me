7eefb40a81e3d16a04b5d4bd6ecb07eef14fd504
jtrtj <jt.r@live.com>
2018-11-28 13:40:13 -0700

    Write test for insert and count

--- spec/complete_me_spec.rb
+++ spec/complete_me_spec.rb
@@ -1,11 +1,22 @@
 require 'spec_helper'
 
 describe CompleteMe do
+  it 'exists' do
+    completion = CompleteMe.new
+
+    expect(completion).to be_a(CompleteMe)
+  end
+
   context 'instance methods' do
-    it 'exists' do
+    it '#insert - adds string to @list and #count returns the count of strings in @list' do
       completion = CompleteMe.new
 
-      expect(completion).to be_a(CompleteMe)
+      expect(completion.insert("pizza").to change(completion.count).by(1))
     end
   end
 end
+
+# completion.insert("pizza")
+
+# completion.count
+# => 1
